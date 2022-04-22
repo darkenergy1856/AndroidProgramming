81407f2c7d4a57914fcc44582b46c70922c77c9e
darkenergy1856 <aryanchandra7360@gmail.com>
2022-04-23 00:44:21 +0530

    My bad understood the question wrong

--- Program_02/app/src/main/java/com/darkenergy/program_2/MainActivity.kt
+++ Program_02/app/src/main/java/com/darkenergy/program_2/MainActivity.kt
@@ -1,33 +1,42 @@
 package com.darkenergy.program_2
 
 import android.os.Bundle
-import android.util.TypedValue
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        var data = 0
+
         val increase = findViewById<Button>(R.id.increase)
         val decrease = findViewById<Button>(R.id.decrease)
         val reset = findViewById<Button>(R.id.reset)
         val textView = findViewById<TextView>(R.id.textView)
 
+        textView.textSize = 50F
+
+        textView.text = "0"
+
         increase.setOnClickListener {
-            textView.setTextSize(TypedValue.COMPLEX_UNIT_PX, textView.textSize + 10F)
+            data++
+            val temp =  data.toString()
+            textView.text = temp
         }
 
         decrease.setOnClickListener {
-            textView.setTextSize(TypedValue.COMPLEX_UNIT_PX, textView.textSize - 10F)
-            if(textView.textSize < 10F)
-                textView.setTextSize(TypedValue.COMPLEX_UNIT_PX , 0F)
+            data --
+            val temp =  data.toString()
+            textView.text = temp
         }
 
         reset.setOnClickListener {
-            textView.setTextSize(TypedValue.COMPLEX_UNIT_PX, 0F)
+            data = 0
+            textView.text = "0"
         }
 
     }
